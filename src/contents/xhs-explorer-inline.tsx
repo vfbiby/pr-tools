3a45d0ace369f04c833bfc947dc00738d8ba0b41
James Zhang <liulfbaby@gmail.com>
2024-08-29 22:05:30 +0800

    fix: pgy href

--- src/contents/xhs-explorer-inline.tsx
+++ src/contents/xhs-explorer-inline.tsx
@@ -1,4 +1,5 @@
 import type {PlasmoCSConfig, PlasmoGetInlineAnchor} from "plasmo"
+import {useEffect, useState} from "react";
 
 export const config: PlasmoCSConfig = {
   matches: ["https://www.xiaohongshu.com/explore/*"]
@@ -8,17 +9,26 @@ export const getInlineAnchor: PlasmoGetInlineAnchor = () =>
   document.querySelector('#noteContainer .author-container .author-wrapper .info')
 
 // Use this to optimize unmount lookups
-export const getShadowHostId = () => "plasmo-inline-example-unique-id"
+export const getShadowHostId = () => "xhs-explorer-inline-unique-id"
 
 const XhsExplorerInline = () => {
+  const [bloggerId, setBloggerId] = useState("")
+
+  useEffect(() => {
+    let href = document.querySelector("#noteContainer .author-container .author-wrapper .info>a").getAttribute("href")
+    let urlArr = href.split('?')[0].split('/');
+    let id = urlArr[urlArr.length - 1];
+    setBloggerId(id)
+  }, [])
+
   return (
     <div
       style={{
         borderRadius: 4,
-        padding: 4,
+        padding: 8,
         background: "pink"
       }}>
-      <a href="https://pgy.xiaohongshu.com/solar/pre-trade/blogger-detail/667a7a3a00000000070047c5" target="_blank">
+      <a href={`https://pgy.xiaohongshu.com/solar/pre-trade/blogger-detail/${bloggerId}`} target="_blank">
         蒲公英首页
       </a>
     </div>
