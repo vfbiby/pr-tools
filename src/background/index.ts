9ee920105fe96ec68ef9b93215035ffe7ab3ee69
James Zhang <liulfbaby@gmail.com>
2024-09-07 20:39:50 +0800

    do same refactor

--- src/background/index.ts
+++ src/background/index.ts
@@ -2,105 +2,89 @@ export {}
 
 console.log(new Date().getSeconds())
 
-const POST = chrome.declarativeNetRequest.RequestMethod.POST;
+const post = chrome.declarativeNetRequest.RequestMethod.POST;
 const MODIFY_HEADERS = chrome.declarativeNetRequest.RuleActionType.MODIFY_HEADERS;
 const SET = chrome.declarativeNetRequest.HeaderOperation.SET;
 const OPTIONS = chrome.declarativeNetRequest.RequestMethod.OPTIONS;
-const XMLHTTPREQUEST = chrome.declarativeNetRequest.ResourceType.XMLHTTPREQUEST;
+const xmlhttprequest = chrome.declarativeNetRequest.ResourceType.XMLHTTPREQUEST;
+const APPEND = chrome.declarativeNetRequest.HeaderOperation.APPEND;
+const thirdParty = chrome.declarativeNetRequest.DomainType.THIRD_PARTY;
 
-let APPEND = chrome.declarativeNetRequest.HeaderOperation.APPEND;
-const testRules = [
-  {
-    id: 1,
-    priority: 11,
-    action: {
-      "type": MODIFY_HEADERS,
-      "requestHeaders": [
-        {
-          "operation": SET,
-          "header": "Origin",
-          "value": "https://www.kdocs.cn"
-        },
-      ],
-      responseHeaders: [
-        {
-          "operation": SET,
-          "header": "Access-Control-Allow-Origin",
-          "value": "*"
-        },
-      ]
-    },
-    condition: {
-      urlFilter: "||www.kdocs.cn",
-      requestMethods: [POST],
-      resourceTypes: [XMLHTTPREQUEST]
-    }
+const ruleForPost = {
+  id: 1,
+  priority: 11,
+  action: {
+    "type": MODIFY_HEADERS,
+    "requestHeaders": [
+      {
+        "operation": SET,
+        "header": "Origin",
+        "value": "https://www.kdocs.cn"
+      },
+    ],
+    responseHeaders: [
+      {
+        "operation": SET,
+        "header": "Access-Control-Allow-Origin",
+        "value": "*"
+      },
+    ]
   },
-  {
-    id: 3,
-    priority: 3,
-    "action": {
-      "type": MODIFY_HEADERS,
-      "responseHeaders": [
-        {
-          "operation": SET,
-          "header": "Access-Control-Allow-Origin",
-          "value": "https://www.xiaohongshu.com"
-        },
-      ],
-      "requestHeaders": [
-        {
-          "operation": SET,
-          "header": "Origin",
-          "value": "https://www.kdocs.cn"
-        },
-      ]
-    },
-    condition: {
-      urlFilter: "||www.kdocs.cn",
-      requestMethods: [OPTIONS],
-      resourceTypes: [XMLHTTPREQUEST]
-    }
-  },
-  {
-    id: 4,
-    priority: 9,
-    "action": {
-      "type": MODIFY_HEADERS,
-      "responseHeaders": [
-        {
-          "operation": APPEND,
-          "header": "Access-Control-Allow-Headers",
-          "value": "airscript-token,content-type"
-        },
-        {
-          "operation": SET,
-          "header": "Access-Control-Allow-Methods",
-          "value": "*"
-        }
-      ]
-    },
-    condition: {
-      urlFilter: "||www.kdocs.cn",
-      requestMethods: [OPTIONS],
-      resourceTypes: [XMLHTTPREQUEST]
-    }
+  condition: {
+    urlFilter: "||www.kdocs.cn",
+    domainType: thirdParty,
+    requestMethods: [post],
+    resourceTypes: [xmlhttprequest]
   }
-];
+};
 
+const ruleForOption = {
+  id: 4,
+  priority: 9,
+  "action": {
+    "type": MODIFY_HEADERS,
+    "responseHeaders": [
+      {
+        "operation": APPEND,
+        "header": "Access-Control-Allow-Headers",
+        "value": "airscript-token,content-type"
+      },
+      {
+        "operation": SET,
+        "header": "Access-Control-Allow-Origin",
+        "value": "https://www.xiaohongshu.com"
+      },
+      {
+        "operation": SET,
+        "header": "Access-Control-Allow-Methods",
+        "value": "*"
+      }
+    ],
+    "requestHeaders": [
+      {
+        "operation": SET,
+        "header": "Origin",
+        "value": "https://www.kdocs.cn"
+      },
+    ]
+  },
+  condition: {
+    urlFilter: "||www.kdocs.cn",
+    requestMethods: [OPTIONS],
+    domainType: thirdParty,
+    resourceTypes: [xmlhttprequest]
+  }
+};
+
+const addRules = [ruleForPost, ruleForOption];
 
 async function applyRules() {
-// Get arrays containing new and old rules
-  const newRules = testRules
-//   const newRules = requestRules
+  const newRules = addRules
   const oldRules = await chrome.declarativeNetRequest.getDynamicRules();
-  console.log(oldRules)
   const oldRuleIds = oldRules.map(rule => rule.id);
 
-// Use the arrays to update the dynamic rules
-  chrome.declarativeNetRequest.updateDynamicRules({
+  await chrome.declarativeNetRequest.updateDynamicRules({
     removeRuleIds: oldRuleIds,
-    // addRules: [],
     addRules: newRules,
   });
 }
