cfe35008c94ecfadb8720fca1785710b2010b801
James Zhang <liulfbaby@gmail.com>
2024-09-15 17:36:47 +0800

    notes rate data display as table

--- src/columns/notes-rate-columns.tsx
+++ src/columns/notes-rate-columns.tsx
@@ -1,7 +1,6 @@
 import type {GridColDef, GridRenderCellParams} from "@mui/x-data-grid-premium";
-import type {IBloggerInfo} from "~src/columns/BloggerInfo";
 
-export const columns: GridColDef<IBloggerInfo>[] = [
+export const columns: GridColDef<NotesRate>[] = [
   {
     field: 'userId', headerName: 'ID', width: 90,
     renderCell: (params: GridRenderCellParams<any, string>) => {
@@ -21,19 +20,31 @@ export const columns: GridColDef<IBloggerInfo>[] = [
   {
     field: 'videoNoteNumber',
     headerName: '视频笔记数',
-    width: 80,
+    width: 100,
     editable: true,
   },
   {
     field: 'hundredLikePercent',
     headerName: '百赞笔记数',
-    width: 80,
+    width: 100,
+    editable: true,
+  },
+  {
+    field: 'videoFullViewRate',
+    headerName: '视频完播率',
+    width: 100,
+    editable: true,
+  },
+  {
+    field: 'videoFullViewBeyondRate',
+    headerName: '视频完播超越',
+    width: 100,
     editable: true,
   },
   {
     field: 'thousandLikePercent',
     headerName: '千赞笔记数',
-    width: 80,
+    width: 100,
     editable: true,
   },
   {
@@ -45,85 +56,85 @@ export const columns: GridColDef<IBloggerInfo>[] = [
   {
     field: 'impMedian',
     headerName: '曝光中位数',
-    width: 80,
+    width: 100,
     editable: true,
   },
   {
     field: 'impMedianBeyondRate',
     headerName: '曝光超越',
-    width: 80,
+    width: 100,
     editable: true,
   },
   {
     field: 'readMedian',
     headerName: '阅读中位数',
-    width: 80,
+    width: 100,
     editable: true,
   },
   {
     field: 'readMedianBeyondRate',
     headerName: '阅读超越',
-    width: 80,
+    width: 100,
     editable: true,
   },
   {
-    field: 'interactionMedia',
+    field: 'mEngagementNum',
     headerName: '互动中位数',
-    width: 80,
+    width: 100,
     editable: true,
   },
   {
-    field: 'interactionRate',
-    headerName: '互动率',
-    width: 80,
-    editable: true,
-  },
-  {
-    field: 'interactionBeyondRate',
-    headerName: '互动超越',
-    width: 80,
+    field: 'mFollowCnt',
+    headerName: '关注中位数',
+    width: 100,
     editable: true,
   },
   {
     field: 'likeMedian',
     headerName: '点赞中位数',
-    width: 80,
+    width: 100,
     editable: true,
   },
   {
     field: 'collectMedian',
     headerName: '收藏中位数',
-    width: 80,
+    width: 100,
     editable: true,
   },
   {
     field: 'commentMedian',
     headerName: '曝光中位数',
-    width: 80,
+    width: 100,
     editable: true,
   },
   {
     field: 'shareMedian',
     headerName: '分享中位数',
-    width: 80,
+    width: 100,
     editable: true,
   },
   {
-    field: 'videoFullViewRate',
-    headerName: '视频完播率',
-    width: 80,
+    field: 'interactionMedian',
+    headerName: '互动中位数(老)',
+    width: 100,
     editable: true,
   },
   {
-    field: 'videoFullViewBeyondRate',
-    headerName: '视频完播超越',
-    width: 80,
+    field: 'interactionRate',
+    headerName: '互动率',
+    width: 100,
+    editable: true,
+  },
+  {
+    field: 'interactionBeyondRate',
+    headerName: '互动超越',
+    width: 100,
     editable: true,
   },
   {
     field: 'picture3sViewRate',
     headerName: '图片3秒完播',
-    width: 80,
+    width: 100,
     editable: true,
   },
   {
@@ -133,15 +144,9 @@ export const columns: GridColDef<IBloggerInfo>[] = [
     editable: true,
   },
   {
-    field: 'mEngagementNum',
-    headerName: '互动中位数',
-    width: 80,
-    editable: true,
-  },
-  {
-    field: 'mFollowCnt',
-    headerName: '关注中位数',
-    width: 80,
+    field: 'createdAt',
+    headerName: '添加时间',
+    width: 120,
     editable: true,
   },
 ];
--- src/components/notes-rate-table.tsx
+++ src/components/notes-rate-table.tsx
@@ -1,5 +1,8 @@
 import React, {type Dispatch, type SetStateAction, useEffect, useState} from "react";
 import {sendToBackground} from "@plasmohq/messaging";
+import {zhCN} from "~src/localization/zh-CN";
+import {DataGridPremium, GridToolbar} from "@mui/x-data-grid-premium";
+import {columns} from "~src/columns/notes-rate-columns";
 
 export function getNotesRateByMessage(setNotesRateInfo: Dispatch<SetStateAction<NotesRate[]>>) {
   sendToBackground({
@@ -17,9 +20,32 @@ export const NotesRateTable = () => {
 
   useEffect(() => {
     getNotesRateByMessage(setNotesRate);
-  }, []);
+  }, [window.location.href]);
 
   return <React.Fragment>
-    <div>{JSON.stringify(notesRate)}</div>
+    <DataGridPremium
+      localeText={zhCN.components.MuiDataGrid.defaultProps.localeText}
+      slots={{toolbar: GridToolbar}}
+      slotProps={{toolbar: {excelOptions: {disableToolbarButton: true}}}}
+      getRowId={row => row.userId}
+      rows={notesRate}
+      initialState={{
+        density: 'comfortable',
+        pinnedColumns: {left: ['name'], right: ['createdAt']},
+        sorting: {
+          sortModel: [{
+            field: 'createdAt', sort: 'desc'
+          }]
+        },
+        pagination: {
+          paginationModel: {pageSize: 20}
+        }
+      }}
+      columns={columns}
+      pagination={true}
+      pageSizeOptions={[5, 10, 20, 50, 100, {value: 1000, label: '1千'}]}
+      checkboxSelection
+      disableRowSelectionOnClick
+    />
   </React.Fragment>
 }
