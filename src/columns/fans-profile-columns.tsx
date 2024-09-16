db12fd8936f182972240721f82602d9e2857df73
James Zhang <liulfbaby@gmail.com>
2024-09-16 09:25:14 +0800

    extract renders of fans profile to renders file

--- src/columns/fans-profile-columns.tsx
+++ src/columns/fans-profile-columns.tsx
@@ -1,52 +1,24 @@
-import type {GridColDef, GridRenderCellParams} from "@mui/x-data-grid-premium";
+import type {GridColDef} from "@mui/x-data-grid-premium";
 import {
+  agesRender,
   bloggerNameRender,
-  IDRender
+  cityRender,
+  deviceRender,
+  genderRender,
+  IDRender,
+  interestRender,
+  provinceRender
 } from "~src/libs/renders";
-import type {
-  FansProfile, FansProfileAge, FansProfileCity, FansProfileDevice,
-  FansProfileGender, FansProfileInterest, FansProfileProvince
-} from "~src/columns/FansProfile";
-import {Box} from "@mui/material";
+import type {FansProfile} from "~src/columns/FansProfile";
 import React from "react";
 
-const PercentageSimple = ({value}: { value: number }) => {
+export const PercentageSimple = ({value}: { value: number }) => {
   return <React.Fragment>{(value * 100).toFixed(2)}%</React.Fragment>
 }
 
-export const agesRender = (params: GridRenderCellParams<any, FansProfileAge[]>) =>
-  <React.Fragment>{params.value?.slice(0, 3).map((age, index) =>
-    <PercentageIndicator key={index} index={index} name={age.group} value={age.percent}/>)}
-  </React.Fragment>;
-
-export const interestRender = (params: GridRenderCellParams<any, FansProfileInterest[]>) =>
-  <React.Fragment>{params.value?.slice(0, 3).map((interest, index) =>
-    <PercentageIndicator key={index} index={index} name={interest.name} value={interest.percent}/>)}
-  </React.Fragment>;
-
-export const provinceRender = (params: GridRenderCellParams<any, FansProfileProvince[]>) =>
-  <React.Fragment>{params.value?.slice(0, 3).map((province, index) =>
-    <PercentageIndicator key={index} index={index} name={province.name} value={province.percent}/>)}
-  </React.Fragment>
-
-export const cityRender = (params: GridRenderCellParams<any, FansProfileCity[]>) =>
-  <React.Fragment>{params.value?.slice(0, 3).map((city, index) =>
-    <PercentageIndicator key={index} index={index} name={city.name} value={city.percent}/>)}
-  </React.Fragment>
-
-function PercentageIndicator({name, value, index}: { name: string, value: number, index: number }) {
+export function PercentageIndicator({name, value, index}: { name: string, value: number, index: number }) {
   return <span>{name}: <PercentageSimple value={value}/>{index < 2 ? ("，") : ("")}</span>;
 }
-
-export const deviceRender = (params: GridRenderCellParams<any, FansProfileDevice[]>) =>
-  <React.Fragment>{params.value?.slice(0, 3).map((device, index) =>
-    <PercentageIndicator key={index} index={index} name={device.desc} value={device.percent}/>)}
-  </React.Fragment>
-
-export const genderRender = (params: GridRenderCellParams<any, FansProfileGender>) =>
-  <React.Fragment>
-    <span>女性：<PercentageSimple value={params.value.female}/>，男性：<PercentageSimple value={params.value.male}/></span>
-  </React.Fragment>
 
 export const columns: GridColDef<FansProfile>[] = [
   {
