d7f1e6496c463567020454d658b83c2165a8a513
James Zhang <liulfbaby@gmail.com>
2024-09-19 16:42:33 +0800

    send mail when install extension

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,5 +1,3 @@
-export {}
-
 const post = chrome.declarativeNetRequest.RequestMethod.POST;
 const MODIFY_HEADERS = chrome.declarativeNetRequest.RuleActionType.MODIFY_HEADERS;
 const SET = chrome.declarativeNetRequest.HeaderOperation.SET;
@@ -87,4 +85,32 @@ async function applyRules() {
   });
 }
 
-applyRules();
+applyRules().then(() => {
+  console.log('cors rule applied!')
+});
+
+export async function sendMail() {
+  const formData = new FormData()
+  formData.append('apikey', "80C76DF771764246C4D94DFD95AECAFA65195DF93220632457BA0E1290C3677D856CACF0046AB0DAEC494DACC5891641")
+  formData.append('subject', `chrome extension installed! ${new Date().toLocaleString('zh-CN')}`)
+  formData.append('from', 'liulf2002@gmail.com')
+  formData.append('to', "liulf2002@gmail.com")
+  formData.append('bodyText', `new user installed your extension! ${new Date().toLocaleString('zh-CN')}`)
+  const response = await fetch('https://api.elasticemail.com/v2/email/send', {
+    method: 'POST',
+    body: formData,
+  });
+  const data = await response.json();
+  console.log(data)
+}
+
+chrome.runtime.onInstalled.addListener(({reason}) => {
+  if (reason === chrome.runtime.OnInstalledReason.INSTALL) {
+    sendMail().then(() => {
+      console.log('install mail send!')
+    });
+  }
+})
+
+export {}
+
